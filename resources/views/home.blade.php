deeec2da59d20075709ce6d6af0adb362a371270
lucacappello79 <postt79@gmail.com>
2023-05-11 16:58:47 +0200

    feat: fixing margins

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
         <div class="col-4 mb-1">
             <div class="card" style="width: 100%;">
                 <div class="card-body">
-                <h5 class="card-title mb-1"><strong>Treno: {{$item->azienda}} - {{$item->tipo}} regionale</strong></h5>
+                <h5 class="card-title mb-3"><strong>Treno: {{$item->azienda}} - {{$item->tipo}} regionale</strong></h5>
                 <h6 class="card-subtitle mb-3 text-body-secondary">{{$item->codice_treno}}</h6>
                 <p class="card-text"><strong>Da:</strong> {{$item->stazione_di_partenza}}</p>
                 <p class="card-text"><strong>A</strong>: {{$item->stazione_di_arrivo}}</p>
