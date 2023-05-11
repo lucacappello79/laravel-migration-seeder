15a9e22850878fcb3390ad5a213b49edf8df3c7c
lucacappello79 <postt79@gmail.com>
2023-05-11 15:33:00 +0200

    feat: creating table

---
+++ database/migrations/2023_05_11_125320_create_trains_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('azienda', 50);
+            $table->string('stazione_di_partenza', 50);
+            $table->string('stazione_di_arrivo', 50);
+            $table->time('orario_di_partenza',);
+            $table->time('orario_di_arrivo');
+            $table->string('codice_treno', 10);
+            $table->tinyInteger('numero_carrozze')->nullable();
+            $table->boolean('in_orario')->nullable();
+            $table->boolean('cancellato')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
