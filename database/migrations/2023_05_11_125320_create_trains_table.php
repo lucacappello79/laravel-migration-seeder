6b6a67b2e3acd01c05f8b6b97b33871cb578dacc
lucacappello79 <postt79@gmail.com>
2023-05-11 15:51:46 +0200

    feat: rolling back, migrating, creating new column

--- database/migrations/2023_05_11_125320_create_trains_table.php
+++ database/migrations/2023_05_11_125320_create_trains_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
 
             $table->string('azienda', 100);
             $table->string('stazione_di_partenza', 100);
-            $table->string('stazione_di_arrivo', 50);
+            $table->string('stazione_di_arrivo', 100);
             $table->time('orario_di_partenza',);
             $table->time('orario_di_arrivo');
             $table->string('codice_treno', 20);
